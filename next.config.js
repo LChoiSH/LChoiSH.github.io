cb4e20ce647c77c6b0e1ab730845e1f39fdc2a4f
Seonghoon Choi <seonghoon.choi@nts-corp.com>
2023-02-26 22:10:39 +0900

    chore: for io hosting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.NODE_ENV === "production"
+    ? "https://lchoish.github.io/portfolio" : "",
 }
 
 module.exports = nextConfig
