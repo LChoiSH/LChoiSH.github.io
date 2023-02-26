bd50bbf8c2c597d60d149a6f98afaf946698a8c1
Seonghoon Choi <seonghoon.choi@nts-corp.com>
2023-02-27 00:46:10 +0900

    deploy to gh-pages

---
+++ out/_next/static/NU5BuQLf10APIh-Oa_6e0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/d5f94ac1d7b3ff0a.css","static/chunks/pages/index-22aac35c72808611.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
