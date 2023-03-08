68b8899de689b241f7542d0165e856544db34c2f
Seonghoon Choi <seonghoon.choi@nts-corp.com>
2023-03-08 18:39:45 +0900

    feat: main project

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,23 @@ import type { AppProps } from 'next/app'
 import { PortfolioProvider } from "../../context/context"
 import { prefix } from "../../config/config"
 import Header from "../components/Header";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
 
   return (
       <PortfolioProvider value={{ prefix }}>
-          <div className="wrap"></div>
-        <Header />
-        <Component {...pageProps} />
+          <Head>
+              <title>Seonghoon Choi Portfolio</title>
+              <meta name="viewport" content="width=device-width, initial-scale=1" />
+              <link rel="icon" href="/favicon.ico" />
+          </Head>
+          <div className="wrap">
+            <Header />
+              <main>
+                <Component {...pageProps} />
+              </main>
+          </div>
       </PortfolioProvider>
   )
 }
